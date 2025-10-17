42a73b8d6759512f4f94504aa29cd8b49a83c722
syedhuzaifa2627 <huzaifasyed2627@gmail.com>
2025-10-18 00:45:48 +0500

    done

---
+++ app.js
@@ -0,0 +1,56 @@
+// Chapter 58 to 67
+
+// Question 1
+
+// // part (1)
+// let mainContent = document.getElementById("mainContent")
+// console.log(mainContent)
+
+// // part(2)
+// console.log(mainContent.children)
+
+// // part(3)
+// let renderContent = document.getElementsByClassName("render")
+
+// for(let i = 0; i<renderContent.length; i++){
+//    console.log(renderContent[i].innerHTML)
+// }
+
+// // part(4)
+// let firstName = document.getElementById("firstName").value = "Huzaifa"
+// console.log(firstName)
+
+// //part(5)
+// let lastName = document.getElementById("lastName").value = "Syed"
+// let email = document.getElementById("email").value = "huzaifasyed@gmail.com"
+// console.log(lastName)
+// console.log(email)
+
+
+// Question 2
+
+// // part(1)
+// let formContent = document.getElementById("formContent")
+// console.log(formContent.nodeType)
+
+// // part(2)
+// let lastName = document.getElementById("lastName")
+// console.log(lastName.nodeType)
+// console.log(lastName.childNodes.nodeType)
+
+// // part(3)
+// console.log(lastName.childNodes.nodeValue = "Last Name: Smith")
+
+// // part(4)
+// let mainContent = document.getElementById("mainContent")
+// console.log(mainContent.firstElementChild)
+// console.log(mainContent.lastElementChild)
+
+// // part(5)
+// console.log(lastName.nextElementSibling)
+// console.log(lastName.previousElementSibling)
+
+// // part(6)
+// let email = document.getElementById("email");
+// console.log(email.parentNode);
+// console.log(email.parentNode.nodeType)
